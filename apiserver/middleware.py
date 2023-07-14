3dd525f1035df6ddffd3d597b03eef2116e0c9df
YU HAN GOH <gohyuhan12345@gmail.com>
2023-07-14 12:40:42 +0800

    Update middleware.py

--- apiserver/middleware.py
+++ apiserver/middleware.py
@@ -8,8 +8,9 @@ class TimezoneMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        ip_address = request.META.get('REMOTE_ADDR')
-        g = geocoder.ip(ip_address)
+        print("ip_address for request: {}".format(request.META.get('HTTP_X_FORWARDED_FOR')))
+        print("ip_address for request: {}".format(request.META.get('REMOTE_ADDR')))
+        g = geocoder.ip(request.META.get('HTTP_X_FORWARDED_FOR'))
 
         if g and g.timezone:
             timezone_name = g.timezone
@@ -20,7 +21,7 @@ class TimezoneMiddleware:
 
         request.timezone = timezone_obj
         request.time = datetime.now().astimezone(request.timezone)
-
+        print("client timezone: {}".format(request.time))
         response = self.get_response(request)
 
         return response
