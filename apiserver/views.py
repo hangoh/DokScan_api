635d853094f35c5c75179455972a247f72146e7f
YU HAN GOH <gohyuhan12345@gmail.com>
2023-07-14 22:07:22 +0800

    Update views.py

--- apiserver/views.py
+++ apiserver/views.py
@@ -61,13 +61,13 @@ def return_scaned_doc(request):
 
     # convert the warped image to grayscale, then threshold it
     # to give it that 'black and white' paper effect
-       '''
+    '''
     warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
     T = threshold_local(warped, 11, offset=10, method="gaussian")
     warped = (warped > T).astype("uint8") * 255
     kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
     warped = cv2.filter2D(warped, -1, kernel)
-        '''
+    '''
     grayscale_image = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
     blurred_image = cv2.GaussianBlur(grayscale_image, (5, 5), 0)
     thresholded_image = cv2.threshold(blurred_image, 127, 255, cv2.THRESH_BINARY)
